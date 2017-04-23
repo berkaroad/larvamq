947c14a2968c707f12e5445a8674bde707fcebb4
Jerry Bai <chinaren_bjr@163.com>
2017-04-23 16:29:20 +0800

    安装脚本

---
+++ install.bat
@@ -0,0 +1,3 @@
+go install github.com/berkaroad/larvamq/larvamqd
+go install github.com/berkaroad/larvamq/larvamq_productor
+go install github.com/berkaroad/larvamq/larvamq_consumer
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+go install github.com/berkaroad/larvamq/larvamqd
+go install github.com/berkaroad/larvamq/larvamq_productor
+go install github.com/berkaroad/larvamq/larvamq_consumer
+
